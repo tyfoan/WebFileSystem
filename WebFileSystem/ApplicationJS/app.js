3fc448f917ac7a8c5a7a30f7bc7ec9c6e4c74b3c
tyfoan <myonlygamess@gmail.com>
2016-09-08 13:33:10 +0300

    Files are seen in each directory

--- WebFileSystem/ApplicationJS/app.js
+++ WebFileSystem/ApplicationJS/app.js
@@ -1,23 +1,31 @@
 ﻿var app = angular.module("WebFileSystem", []);
 
 app.controller("HomeController", ["$scope", "$http", function ($scope, $http) {
+    $scope.currentPath = "";
+    $scope.previousPath = "";
 
-    $http.get('/api/values').success(function (data) {
-        console.log(data);
-        $scope.result = data;
-    }).error(function () {
-        alert('oops something went wrong');
-    });
+    $scope.getBack = function () {
+        $scope.previousPath = $scope.currentPath.split("\\");
 
-    $scope.currentPath = "/";
+        if ($scope.previousPath.length == 2 && !$scope.previousPath[1]) {
+            $scope.previousPath.pop();
+        }
 
-    $scope.backToNode = function () {
-        
-    }
+        $scope.previousPath.pop();
+        var drive = $scope.previousPath.shift();
 
+        if (!drive)
+            drive = "";
+        else
+            drive += "\\";
 
-    $scope.goToNode = function (path) {
+        $scope.previousPath = $scope.previousPath.join("\\");
+        $scope.getPath(drive + $scope.previousPath);
+    };
+
+    $scope.getPath = function (path) {
         $scope.currentPath = path;
+
         $http.get("/api/values?path=" + encodeURIComponent(path))
             .success(function (data) {
                 $scope.result = data;
@@ -25,7 +33,9 @@ app.controller("HomeController", ["$scope", "$http", function ($scope, $http) {
             }).error(function () {
                 alert('oops something went wrong');
             });
-    }
+    };
+
+
+
+    $scope.getPath($scope.currentPath);
 }]);
-
-
--- WebFileSystem/Controllers/ValuesController.cs
+++ WebFileSystem/Controllers/ValuesController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Web.Http;
 
 namespace WebFileSystem.Controllers
@@ -12,43 +14,70 @@ namespace WebFileSystem.Controllers
     public class DirToView
     {
         public string FullPath { get; set; }
-        public string LastSegment { get; set; }
+        public string FileName { get; set; }
     }
 
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<DirToView> Get()
-        {
-            var drives = DriveInfo.GetDrives().Select(x => x.Name).ToList();
-            var directory = new List<DirToView>();
 
-            foreach (var drive in drives)
+        public static string ExtractFilename(string filepath)
+        {
+            // If path ends with a "\", it's a path only so return String.Empty.
+            if (filepath.Trim().EndsWith(@"\"))
+                return filepath;
+
+            //if (Regex.Matches(filepath, @"\").Count <= 2)
+            //    return filepath;
+
+            // Determine where last backslash is.
+            int position = filepath.LastIndexOf('\\');
+            // If there is no backslash, assume that this is a filename.
+            if (position == -1)
             {
-                directory.Add(new DirToView() { LastSegment = drive, FullPath = drive });
+                // Determine whether file exists in the current directory.
+                if (File.Exists(Environment.CurrentDirectory + Path.DirectorySeparatorChar + filepath))
+                    return filepath;
+                else
+                    return String.Empty;
             }
-            return directory;
+            else
+            {
+                // Return filename without file path.
+                return filepath.Substring(position + 1);
+            }
         }
 
 
         // GET api/values/5
-        public IEnumerable<DirToView> Get(string path)
+        public object Get(string path)
         {
+            var dirs = new List<DirToView>();
 
-            var directories = Directory.GetDirectories(path);
-            var list = new List<DirToView>();
+            IList<string> directories;
+            IList<string> files;
 
-            foreach (var item in directories)
+            if (string.IsNullOrWhiteSpace(path))
             {
-
-                list.Add(new DirToView()
-                {
-                    FullPath = item,
-                    LastSegment = path.Substring(path.LastIndexOf('/') + 1)
-                });
+                directories = DriveInfo.GetDrives().Select(x => x.Name).ToList();
+                files = null;
+            }
+            else
+            {
+                directories = Directory.GetDirectories(path).Where(x=> !f.;
+                files = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly).ToList();
             }
 
-            return list;
-        } 
+            foreach (var directory in directories)
+            {
+                dirs.Add(new DirToView()
+                {
+                    FullPath = directory,
+                    FileName = ExtractFilename(directory)
+                });
+            }
+            //http://stackoverflow.com/questions/1288975/how-to-test-if-directory-is-hidden-in-c
+            //http://stackoverflow.com/questions/4133544/hidden-folders-c-sharp
+            return new { dirs, files };
+        }
     }
 }
